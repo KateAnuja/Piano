a96ca7f5dd0cf53279ebbde64f8bd80392b8cc00
Anuja Kate <kate.anuja15@gmail.com>
2021-05-14 00:18:46 +0530

    basic note added with sound

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    public platform: Platform, 
+  ) {}
+
+  play(e){
+    console.log(e.target.innerText);
+    let button = e.target.innerText;
+    switch(button){
+      case "C" :
+            this.playPianoNote("/assets/notes/C.mp3");
+            break;
+      case "D" :
+            this.playPianoNote("/assets/notes/D.mp3");
+            break;
+      case "E" :
+              this.playPianoNote("/assets/notes/E.mp3");
+              break;
+      case "F" :
+              this.playPianoNote("/assets/notes/F.mp3");
+              break;
+      case "G" :
+            this.playPianoNote("/assets/notes/G.mp3");
+            break;
+      case "A" :
+            this.playPianoNote("/assets/notes/A.mp3");
+            break;
+      case "B" :
+            this.playPianoNote("/assets/notes/B.mp3");
+            break;
+      default : 
+            console.log("default");
+            break;      
+    }
+  }
+
+  playPianoNote(note){
+    let audio = new Audio();
+    audio.src=note;
+    audio.load();
+    audio.play();
+  }
+  
 
 }
